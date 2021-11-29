38510877d1fadf96231b33478b5cd7acc068a182
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-11-28 20:49:34 -0600

    main

---
+++ src/components/FeedbackItem.svelte
@@ -0,0 +1,43 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+	import Card from './ui/card.svelte';
+    export let item;
+    const dispatch = createEventDispatcher();
+
+    const handleDelete = (itemId) => {
+        dispatch('delete-feedback', itemId)
+    }
+    
+</script>
+<Card>
+    <div class='num-display'>
+        {item.rating}
+    </div>
+    <button on:click={() => handleDelete(item.id)} class='close'>X</button>
+    <p class='text-display'>
+        {item.text}
+    </p>
+</Card>
+<style>
+    .num-display {
+        position: absolute;
+        top: -25px;
+        left: -25px;
+        width: 50px;
+        height: 50px;
+        background: #ff6a95;
+        color: white;
+        border: 1px #eee solid;
+        border-radius: 50%;
+        padding: 15px;
+        text-align: center;
+        font-size: 19px;
+    }
+    .close {
+        position: absolute;
+        right: 1rem;
+        top: 1rem;
+        background: none;
+        border: 0;
+    }
+</style>
